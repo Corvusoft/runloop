5ab67398d83c2e2860e9fffdf046aa9a27e12c61
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2017-05-10 19:50:52 +1000

    Removed repo static.

--- test/acceptance/source/logging.cpp
+++
@@ -1,5 +0,0 @@
-//test throwing erros from error handlers see if it loops.
-//returning errors from error handlers, should go to loggers.
-
-//test throwing exceptiosn from logger.
-//test return error from logger.
--- test/acceptance/source/suspend.cpp
+++
@@ -1,5 +0,0 @@
-//suspend all tasks
-//suspend and cancel a task
-//suspend and add a task
-//suspend and change logger
-//suspend and change error handler
